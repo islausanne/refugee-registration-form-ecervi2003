8a17426076bfa813eef4bb1dfca75a80f539024b
Emiliano Cervi <ecervi@islstudent.ch>
2025-11-25 12:41:30 +0100

    DATE 25/11/2025 Goal for today: Put all of my fields in the code, lables placeholders etc. Attach a POST file Work completed: I did all of my fields and place holders and  did the id for date of birth and lables, I checked If I have my POSt file and OI have it already Did I follow my plan: Yes I did everything I was supposed to complete in this class Next session: Do validation, probably dropdown boxes etc and the jason file to store information.

--- app.py
+++ app.py
@@ -1,17 +1,59 @@
-from flask import Flask
-import os
+from distutils.command.build_scripts import first_line_re
+
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
+import os
+
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # Needed for flash messages
 
+# Home route
 @app.route('/')
-def home():
-    return "Welcome to the Homepage!"
+def index():
+    return render_template('index.html')
+
+# Registration form page
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+# Handle form submission (students will add JSON save code here)
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    first_name = request.form[' first_name']
+    last_name = request.form['last_name']
+    country = request.form['country']
+    date_of_birth = request.form['date_of_birth']
+    gender= request.form['gender']
+    medical_condition= request.form['medical_condition']
+    family_size= request.form['family size']
+    phone_number= request.form['phone number']
+    email= request.form['email']
 
 
 
-@app.route('/contact')
-def contact():
-    return "Contact us at: info@example.com."
+    # Check if file exists
+    if os.path.exists('registrations.json'):
+        with open('registrations.json', 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
 
+    # Add the new registration
+    data.append({'first_name': first_name,'last_name':last_name, 'country': country, 'date of birth': date_of_birth,'gender':gender, 'medical condition':medical_condition,'family size':family_size,'phone number':phone_number,'email':email })
+
+    # Save all registrations back to the file
+    with open('registrations.json', 'w') as file:
+        json.dump(data, file, indent=2)
+
+    return redirect(url_for('index'))
+
+# Display stored registrations (students will add JSON reading code here)
+@app.route('/view')
+def view_registrations():
+    with open('registrations.json', 'r') as file:
+        data = json.load(file)
+    return render_template('view.html', registrations=data)
 if __name__ == '__main__':
     app.run(debug=True)
+
